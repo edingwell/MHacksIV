1dd9359477afe92036761383decbadde577471ce
edingwell <dingem@umich.edu>
2014-09-06 02:10:20 -0400

    Added Transition Helper, beginning of data, start of framework for homepage

---
+++ MHacks/Dining App/Dining App/Data/GetDummyData.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Storage;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+
+namespace Ding.Data
